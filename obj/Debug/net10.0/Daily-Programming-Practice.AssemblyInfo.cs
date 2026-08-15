e54a1c3143c36ab96ebb1444c67851c6c38b4176
madhurapatil-cs <madhurapatil857@gmail.com>
2026-08-15 12:48:44 +0530

    Added gitignore

--- obj/Debug/net10.0/Daily-Programming-Practice.AssemblyInfo.cs
+++ obj/Debug/net10.0/Daily-Programming-Practice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Daily-Programming-Practice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bb4375494fa7aefa02c578c7327a14e26c4bab4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Daily-Programming-Practice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Daily-Programming-Practice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
